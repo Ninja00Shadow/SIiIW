504f1c6baddbf48f2ce55b756cf01f1419f48559
Dawid <mrshadow813@gmail.com>
2024-03-20 21:49:22 +0100

    move files, implement alternative heuristics

---
+++ ShortestPaths/alternative_heuristics.py
@@ -0,0 +1,199 @@
+import math
+
+from dijsktra import *
+import sys
+
+
+def calculate_number_of_connections(start_edges, end_edges):
+    connections_from_start = 1
+    connections_from_end = 1
+
+    for start_edge in start_edges:
+        connections_from_start += len(start_edge.connections)
+
+    for end_edge in end_edges:
+        connections_from_end += len(end_edge.connections)
+
+    return connections_from_start + connections_from_end
+
+
+def alt_time_heuristic(start_edges, end_edges):
+    return (425 / calculate_number_of_connections(start_edges, end_edges)) * 300
+
+
+def alt_transfers_heuristic(start_stop, end_stop):
+    return math.ceil(425 / calculate_number_of_connections(start_stop, end_stop))
+
+
+def alt_astar(graph, start_stop, end_stop, previous_arrival_time):
+    shortest_distance = {}
+    predecessor = {}
+    unseen_nodes = graph.get_nodes().copy()
+    infinity = sys.maxsize
+    path = []
+    start_stop = graph.get_vertex(start_stop)
+    end_stop = graph.get_vertex(end_stop)
+
+    for node in unseen_nodes:
+        shortest_distance[node] = {
+            'cost': infinity,
+            'departure_arrival': (0, 0, "")
+        }
+    shortest_distance[start_stop]['cost'] = 0
+    shortest_distance[start_stop]['departure_arrival'] = (0, previous_arrival_time, "")
+
+    while unseen_nodes:
+        min_node = min(unseen_nodes,
+                       key=lambda node: shortest_distance[node]['cost'] + alt_time_heuristic(graph.get_edges(start_stop), graph.get_edges(node)))
+        unseen_nodes.pop(min_node)
+
+        for child_node in graph.get_edges(min_node):
+            weight = calculate_time_from_previous_arrival_to_next_arrival(
+                shortest_distance[min_node]['departure_arrival'][1],
+                child_node.connections
+            )
+
+            if weight + shortest_distance[min_node]['cost'] < shortest_distance[child_node.end_stop]['cost']:
+                shortest_distance[child_node.end_stop]['cost'] = weight + shortest_distance[min_node]['cost']
+                predecessor[child_node.end_stop] = {'previous_stop': min_node}
+                shortest_distance[child_node.end_stop]['departure_arrival'] = (
+                    get_closest_connection(child_node.connections,
+                                           shortest_distance[min_node]['departure_arrival'][1])
+                )
+
+            if child_node.end_stop == end_stop:
+                current_node = end_stop
+                while current_node != start_stop:
+                    path.insert(0, {'stop': current_node,
+                                    'connection': shortest_distance[current_node]['departure_arrival']})
+                    current_node = predecessor[current_node]['previous_stop']
+                path.insert(0, {'stop': start_stop, 'connection': shortest_distance[start_stop]['departure_arrival']})
+
+                return path, shortest_distance[end_stop]['cost']
+
+    if shortest_distance[end_stop]['cost'] == infinity:
+        print("Path not reachable")
+        return None
+
+    current_node = end_stop
+    while current_node != start_stop:
+        path.insert(0, {'stop': current_node, 'connection': shortest_distance[current_node]['departure_arrival']})
+        current_node = predecessor[current_node]['previous_stop']
+    path.insert(0, {'stop': start_stop, 'connection': shortest_distance[start_stop]['departure_arrival']})
+
+    return path, shortest_distance[end_stop]['cost']
+
+
+def alt_astar_transfers(graph, start_stop, end_stop, previous_arrival_time):
+    shortest_distance = {}
+    predecessor = {}
+    unseen_nodes = graph.get_nodes().copy()
+    infinity = sys.maxsize
+    path = []
+    start_stop = graph.get_vertex(start_stop)
+    end_stop = graph.get_vertex(end_stop)
+
+    for node in unseen_nodes:
+        shortest_distance[node] = {
+            'transfers': infinity,
+            'departure_arrival': (0, 0, "")
+        }
+    shortest_distance[start_stop]['transfers'] = 0
+    shortest_distance[start_stop]['departure_arrival'] = (0, previous_arrival_time, "")
+
+    while unseen_nodes:
+        min_node = min(unseen_nodes,
+                       key=lambda node: shortest_distance[node]['transfers'] + alt_transfers_heuristic(graph.get_edges(start_stop), graph.get_edges(end_stop)))
+        unseen_nodes.pop(min_node)
+
+        for child_node in graph.get_edges(min_node):
+            next_connection = get_least_transfer_connection(child_node.connections,
+                                                            shortest_distance[min_node]['departure_arrival'])
+
+            transfers = 1 if not next_connection[2] == shortest_distance[min_node]['departure_arrival'][2] else 0
+
+            if transfers + shortest_distance[min_node]['transfers'] < shortest_distance[child_node.end_stop][
+                'transfers']:
+                shortest_distance[child_node.end_stop]['transfers'] = shortest_distance[min_node][
+                                                                          'transfers'] + transfers
+                predecessor[child_node.end_stop] = {'previous_stop': min_node}
+                shortest_distance[child_node.end_stop]['departure_arrival'] = next_connection
+
+            if child_node.end_stop == end_stop:
+                current_node = end_stop
+                while current_node != start_stop:
+                    path.insert(0, {'stop': current_node,
+                                    'connection': shortest_distance[current_node]['departure_arrival']})
+                    current_node = predecessor[current_node]['previous_stop']
+                path.insert(0, {'stop': start_stop, 'connection': shortest_distance[start_stop]['departure_arrival']})
+
+                return path, shortest_distance[end_stop]['transfers']
+
+    if shortest_distance[end_stop]['transfers'] == infinity:
+        print("Path not reachable")
+        return None
+
+    current_node = end_stop
+    while current_node != start_stop:
+        path.insert(0, {'stop': current_node, 'connection': shortest_distance[current_node]['departure_arrival']})
+        current_node = predecessor[current_node]['previous_stop']
+    path.insert(0, {'stop': start_stop, 'connection': shortest_distance[start_stop]['departure_arrival']})
+
+    return path, shortest_distance[end_stop]['transfers']
+
+
+def testTime1(graph):
+    print('For standard time heuristic')
+    start_time = time.time()
+    path, travel_time = astar(graph, 'Miękinia Cegielnia', 'Rdestowa', time_to_seconds('14:16:00'))
+    end_time = time.time()
+
+    sys.stderr.write(f"Travel time: {seconds_to_time(travel_time)}\n")
+    sys.stderr.write(f"Run time: {end_time - start_time}\n")
+    print_path(path)
+
+    print()
+    print('For alternative time heuristic')
+    start_time = time.time()
+    path, travel_time = alt_astar(graph, 'Miękinia Cegielnia', 'Rdestowa',
+                                  time_to_seconds('14:16:00'))
+    end_time = time.time()
+
+    sys.stderr.write(f"Travel time: {seconds_to_time(travel_time)}\n")
+    sys.stderr.write(f"Run time: {end_time - start_time}\n")
+    print_path(path)
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+
+def testTime2(graph):
+    print('For standard transfers heuristic')
+    start_time = time.time()
+    path, number_of_transfers = astar_transfers(graph, 'Krucza', 'Inflancka', time_to_seconds('15:05:00'))
+    end_time = time.time()
+
+    sys.stderr.write(f"Transfers: {number_of_transfers}\n")
+    sys.stderr.write(f"Run time: {end_time - start_time}\n")
+    print_path(path)
+
+    print()
+    print('For alternative transfers heuristic')
+    start_time = time.time()
+    path, number_of_transfers = alt_astar_transfers(graph, 'Krucza', 'Inflancka', time_to_seconds('15:05:00'))
+    end_time = time.time()
+
+    sys.stderr.write(f"Transfers: {number_of_transfers}\n")
+    sys.stderr.write(f"Run time: {end_time - start_time}\n")
+    print_path(path)
+
+    print('---------------------------------------------------------------------------------------------------------')
+
+
+if __name__ == '__main__':
+    proto_graph = read_connection_graph_file('connection_graph.csv')
+    stops_graph = Graph()
+    convert_connection_graph_to_graph(proto_graph, stops_graph)
+
+    testTime1(stops_graph)
+
+    testTime2(stops_graph)
--- ShortestPaths/tests.py
+++ ShortestPaths/tests.py
@@ -1,4 +1,5 @@
 from dijsktra import *
+from alternative_heuristics import alt_astar, alt_astar_transfers
 from graph import Graph
 from csv_reader_converter import convert_connection_graph_to_graph, read_connection_graph_file
 
@@ -12,13 +13,17 @@ def general_test_case(graph, criteria, start_stop, end_stop, start_time):
         path, travel_time = astar(graph, start_stop, end_stop, start_time)
     elif criteria == 'p':
         path, number_of_transfers = astar_transfers(graph, start_stop, end_stop, start_time)
+    elif criteria == 'alt':
+        path, travel_time = alt_astar(graph, start_stop, end_stop, start_time)
+    elif criteria == 'altp':
+        path, number_of_transfers = alt_astar_transfers(graph, start_stop, end_stop, start_time)
     alg_end_time = time.time()
 
-    if criteria == 'dt' or criteria == 'at':
-        sys.stderr.write(f"Travel time: {seconds_to_time(travel_time)}\n")
-    elif criteria == 'p':
-        sys.stderr.write(f"Number of transfers: {number_of_transfers}\n")
-    sys.stderr.write(f"Run time: {alg_end_time - alg_start_time}\n")
+    if criteria == 'dt' or criteria == 'at' or criteria == 'alt':
+        print(f"Travel time: {seconds_to_time(travel_time)}", file=sys.stderr, flush=True)
+    elif criteria == 'p' or criteria == 'altp':
+        print(f"Number of transfers: {number_of_transfers}", file=sys.stderr, flush=True)
+    print(f"Run time: {alg_end_time - alg_start_time}", file=sys.stderr, flush=True)
     print_path(path)
 
     print('---------------------------------------------------------------------------------------------------------')
@@ -86,6 +91,27 @@ def time_transfers_comparison_test_cases(graph):
     general_test_case(graph, 'p', 'Krucza', 'Inflancka', '15:05:00')
 
 
+def alt_astar_test_cases(graph):
+    general_test_case(graph, 'alt', 'Stanki', 'Lubelska', '11:00:00')
+    general_test_case(graph, 'alt', 'Brzezia Łąka - Główna', 'Arkady (Capitol)', '11:15:00')
+    general_test_case(graph, 'alt', 'Jerzmanowo (Cmentarz I)', 'pl. Daniłowskiego', '09:34:00')
+    general_test_case(graph, 'alt', 'rondo Św. Ojca Pio', 'Sowia', '17:16:00')
+    general_test_case(graph, 'alt', 'C.H. Aleja Bielany', 'Ślęza - skrzyżowanie', '16:49:00')
+    general_test_case(graph, 'alt', 'Tczewska', 'Park Wschodni', '14:32:00')
+    general_test_case(graph, 'alt', 'Miękinia Cegielnia', 'Rdestowa', '09:09:00')
+
+
+def alt_astar_transfers_test_cases(graph):
+    general_test_case(graph, 'altp', 'Iwiny - rondo', 'Hala Stulecia', '14:38:00')
+    general_test_case(graph, 'altp', 'Stanki', 'Lubelska', '11:00:00')
+    general_test_case(graph, 'altp', 'Brzezia Łąka - Główna', 'Arkady (Capitol)', '11:15:00')
+    general_test_case(graph, 'altp', 'Jerzmanowo (Cmentarz I)', 'pl. Daniłowskiego', '09:34:00')
+    general_test_case(graph, 'altp', 'rondo Św. Ojca Pio', 'Sowia', '17:16:00')
+    general_test_case(graph, 'altp', 'C.H. Aleja Bielany', 'Ślęza - skrzyżowanie', '16:49:00')
+    general_test_case(graph, 'altp', 'Tczewska', 'Park Wschodni', '14:32:00')
+    general_test_case(graph, 'altp', 'Miękinia Cegielnia', 'Rdestowa', '09:09:00')
+
+
 def average_number_of_connections_in_edges(graph):
     sum_of_connections = 0
     edges = 0
@@ -122,3 +148,15 @@ if __name__ == '__main__':
     print()
 
     time_transfers_comparison_test_cases(stops_graph)
+
+    print()
+    print()
+    print()
+
+    alt_astar_test_cases(stops_graph)
+
+    print()
+    print()
+    print()
+
+    alt_astar_transfers_test_cases(stops_graph)
