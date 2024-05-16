9eaf506e4e0c207f7ef86948d22b592bd5abd018
Dawid <mrshadow813@gmail.com>
2024-05-16 22:49:16 +0200

    better rules, more tests

--- KnowledgeBase/semantic_network.py
+++ KnowledgeBase/semantic_network.py
@@ -32,12 +32,19 @@ class WaterOutlet(Fact):
     damaged = Field(bool, default=False)
     connected_correctly = Field(bool, default=True)
 
+    def is_ok(self):
+        return not any([self.blocked, self.caught, self.jammed, self.connected_too_high, self.water_leak,
+                        self.damaged]) and self.connected_correctly
+
 
 class DrainPipe(Fact):
     blocked = Field(bool, default=False)
     caught = Field(bool, default=False)
     jammed = Field(bool, default=False)
     unapproved_extension = Field(bool, default=False)
+
+    def is_ok(self):
+        return not any([self.blocked, self.caught, self.jammed]) and not self.unapproved_extension
 
 
 class PowerCord(Fact):
@@ -62,108 +69,149 @@ class Program(Fact):
     supports_fabric_softener = Field(bool, default=False)
 
 
-class WaterSystemRules:
-    @Rule(AND(Display(errors=["E:30 / -80"]), OR(DrainPipe(blocked=True), WaterOutlet(blocked=True))))
-    def clean_drainage(self):
-        print("Fix: Clean the drain pipe and the water outlet hose.")
-        self.declare(Display(errors=[]))
-        self.declare(DrainPipe(blocked=False))
-        self.declare(WaterOutlet(blocked=False))
-
-    @Rule(AND(Display(errors=["E:30 / -80"]), OR(OR(DrainPipe(caught=True), DrainPipe(jammed=True)),
-                                                 OR(WaterOutlet(caught=True), WaterOutlet(jammed=True)))))
-    def remove_obstruction(self):
-        print("Fix: Ensure that the drain pipe and water drain hose are not kinked or trapped.")
-        self.declare(Display(errors=[]))
-        self.declare(DrainPipe(caught=False))
-        self.declare(DrainPipe(jammed=False))
-        self.declare(WaterOutlet(caught=False))
-        self.declare(WaterOutlet(jammed=False))
-
-    @Rule(AND(Display(errors=["E:30 / -80"]), WaterOutlet(connected_too_high=True)))
-    def lower_water_outlet(self):
-        print("Fix: Install the water outlet hose at a maximum height of 1 metre.")
-        self.declare(Display(errors=[]))
-        self.declare(WaterOutlet(connected_too_high=False))
-
-    @Rule(AND(Display(errors=["E:30 / -80"]), DrainPipe(unapproved_extension=True)))
-    def remove_extension(self):
-        print("Fix: Remove any unapproved extensions from the water hose. ")
-        self.declare(Display(errors=[]))
-        self.declare(DrainPipe(unapproved_extension=False))
-
-    @Rule(WaterOutlet(water_leak=True, damaged=True))
-    def replace_water_outlet(self):
-        print("Fix: Replace the water outlet.")
-        self.declare(WaterOutlet(water_leak=False, damaged=False))
-
-    @Rule(WaterOutlet(water_leak=True, connected_correctly=False))
-    def replace_drain_pipe(self):
-        print("Fix: Connect the water outlet hose correctly.")
-        self.declare(DrainPipe(blocked=False, connected_correctly=True))
-
-
-class DoorRules:
-    @Rule(Door(closed=False))
-    def check_door(self):
-        print("Fix: Close the door.")
-        # doors_to_close = [f for f in self.facts.values() if isinstance(f, Door) and not f['closed']]
-        # for door in doors_to_close:
-        #     self.retract(door)
-        self.declare(Door(closed=True))
-
-    @Rule(Door(caught_laundry=True))
-    def remove_caught_laundry(self):
-        print("Fix: Remove any trapped laundry")
-        self.declare(Door(caught_laundry=False))
-
-    @Rule(Door(water_leak=True))
-    def fix_water_leak(self):
-        print("Fix: Clean the door and the seal.")
-        self.declare(Door(water_leak=False))
-
-
-class DetergentDrawerRules:
-    @Rule(AND(Display(errors=["E:30/-20"]), DetergentDrawer(too_much_detergent=True)))
-    def remove_excess_detergent(self):
-        print("Fix: Reduce the amount of detergent for the next washing cycle with the same load.")
-        self.declare(Display(errors=[]))
-        self.declare(DetergentDrawer(too_much_detergent=False))
-
-    @Rule(AND(DetergentDrawer(fabric_softener_remaining=True), Program(supports_fabric_softener=False)))
-    def add_fabric_softener(self):
-        print("Fix: Use a program that supports fabric softener.")
-        self.declare(DetergentDrawer(fabric_softener_remaining=False))
-
-
-class SemanticNetwork(WaterSystemRules, DoorRules, DetergentDrawerRules, KnowledgeEngine):
-    @Rule(PowerCord(plugged_in=False))
-    def plug_in_power_cord(self):
+class SemanticNetwork(KnowledgeEngine):
+    @Rule(AS.power_cord << PowerCord(plugged_in=False))
+    def plug_in_power_cord(self, power_cord):
         print("Fix: Plug in the power cord.")
-        self.declare(PowerCord(plugged_in=True))
+        self.modify(power_cord, plugged_in=True)
 
-    @Rule(AND(Display(on=False), Button(name="Start/Pause", flashing=True)))
-    def energy_saving_mode(self):
+    @Rule(AS.display << Display(on=False), AS.button << Button(name="Start/Pause", flashing=True))
+    def energy_saving_mode(self, display, button):
         print("Fix: Energy saving mode is active. Press any button.")
-        print("Confirmation: Display lights up again.")
-        self.declare(Display(on=True))
-        self.declare(Button(name="Start/Pause", flashing=False))
+        self.modify(display, on=True)
+        self.modify(button, flashing=False)
 
-    @Rule(AND(Display(errors=["E:60 / -2B"]), Drum(unbalanced_load=True)))
-    def redistribute_laundry(self):
+    @Rule(
+        AS.display << Display(errors=MATCH.errors),
+        TEST(lambda errors: "E:60 / -2B" in errors),
+        AS.drum << Drum(unbalanced_load=True))
+    def redistribute_laundry(self, display, errors, drum):
         print("Fix: Distribute the laundry evenly in the drum.")
-        self.declare(Display(errors=[]))
-        self.declare(Drum(unbalanced_load=False))
+        self.modify(display, errors=[e for e in errors if e != "E:60 / -2B"])
+        self.modify(drum, unbalanced_load=False)
 
-    @Rule(AND(Display(errors=["E:30/-20"]), Drum(additional_water=True)))
-    def remove_excess_water(self):
-        print("Fix: Do not add any extra water to the appliance while it is operating.")
-        self.declare(Display(errors=[]))
-        self.declare(Drum(additional_water=False))
+    @Rule(
+        AS.display << Display(errors=MATCH.errors),
+        TEST(lambda errors: "E:30/-20" in errors),
+        OR(AS.drum << Drum(additional_water=True), AS.drawer << DetergentDrawer(too_much_detergent=True)))
+    def remove_excess_water(self, display, errors, drum=None, drawer=None):
+        if drum:
+            print("Fix: Do not add any extra water to the appliance while it is operating.")
+            self.modify(drum, additional_water=False)
+        if drawer:
+            print("Fix: Reduce the amount of detergent for the next washing cycle with the same load.")
+            self.modify(drawer, too_much_detergent=False)
 
-    @Rule(Drum(jerking=True))
-    def motor_test(self):
+        self.modify(display, errors=[e for e in errors if e != "E:30/-20"])
+
+    @Rule(AS.drum << Drum(jerking=True))
+    def motor_test(self, drum):
         print("This is caused by an internal motor test. Not a fault – no action required.")
+        self.modify(drum, jerking=False)
+
+    @Rule(AND(
+        AS.display << Display(errors=MATCH.errors),
+        TEST(lambda errors: "E:30 / -80" in errors),
+        OR(
+            AS.pipe_blocked << DrainPipe(blocked=True),
+            AS.hose_blocked << WaterOutlet(blocked=True),
+            AS.pipe_caught << DrainPipe(caught=True),
+            AS.pipe_jammed << DrainPipe(jammed=True),
+            AS.hose_caught << WaterOutlet(caught=True),
+            AS.hose_jammed << WaterOutlet(jammed=True),
+            AS.outlet_high << WaterOutlet(connected_too_high=True),
+            AS.pipe_extension << DrainPipe(unapproved_extension=True)
+        )
+    ))
+    def handle_error_e30_80(self, pipe_blocked=None, hose_blocked=None,
+                            pipe_caught=None, pipe_jammed=None, hose_caught=None,
+                            hose_jammed=None, outlet_high=None, pipe_extension=None):
+        if pipe_blocked:
+            print("Fix: Clean the drain pipe.")
+            self.modify(pipe_blocked, blocked=False)
+
+        if hose_blocked:
+            print("Fix: Clean the water outlet hose.")
+            self.modify(hose_blocked, blocked=False)
+
+        if pipe_caught or pipe_jammed:
+            print("Fix: Ensure that the drain pipe is not kinked or trapped.")
+            if pipe_caught:
+                self.modify(pipe_caught, caught=False)
+            if pipe_jammed:
+                self.modify(pipe_jammed, jammed=False)
+
+        if hose_caught or hose_jammed:
+            print("Fix: Ensure that the water outlet hose is not kinked or trapped.")
+            if hose_caught:
+                self.modify(hose_caught, caught=False)
+            if hose_jammed:
+                self.modify(hose_jammed, jammed=False)
+
+        if outlet_high:
+            print("Fix: Install the water outlet hose at a maximum height of 1 metre.")
+            self.modify(outlet_high, connected_too_high=False)
+
+        if pipe_extension:
+            print("Fix: Remove any unapproved extensions from the water hose.")
+            self.modify(pipe_extension, unapproved_extension=False)
+
+    @Rule(
+        AS.display << Display(errors=MATCH.errors),
+        TEST(lambda errors: "E:30 / -80" in errors),
+        WaterOutlet(blocked=False, caught=False, jammed=False, connected_too_high=False, water_leak=False,
+                    damaged=False, connected_correctly=True),
+        DrainPipe(blocked=False, caught=False, jammed=False, unapproved_extension=False),
+    )
+    def remove_error_e30_80(self, display, errors):
+        errors = [e for e in errors if e != "E:30 / -80"]
+        self.modify(display, errors=errors)
+
+    @Rule(OR(AS.outlet << WaterOutlet(water_leak=True, damaged=True),
+             AS.outlet << WaterOutlet(water_leak=True, connected_correctly=False)))
+    def replace_water_outlet(self, outlet):
+        if outlet["damaged"]:
+            print("Fix: Replace the water outlet.")
+            self.modify(outlet, water_leak=False, damaged=False)
+        else:
+            print("Fix: Connect the water outlet hose correctly.")
+            self.modify(outlet, water_leak=False, connected_correctly=True)
+
+    @Rule(AS.door << Door(closed=False))
+    def check_door(self, door):
+        print("Fix: Close the door.")
+        self.modify(door, closed=True)
+
+    @Rule(AS.door << Door(caught_laundry=True))
+    def remove_caught_laundry(self, door):
+        print("Fix: Remove any trapped laundry")
+        self.modify(door, caught_laundry=False)
+
+    @Rule(AS.door << Door(water_leak=True))
+    def fix_water_leak(self, door):
+        print("Fix: Clean the door and the seal.")
+        self.modify(door, water_leak=False)
+
+    @Rule(AS.drawer << DetergentDrawer(fabric_softener_remaining=True),
+          AS.prog << Program(supports_fabric_softener=False))
+    def add_fabric_softener(self, drawer, prog):
+        print("Fix: Use a program that supports fabric softener.")
+        self.modify(drawer, fabric_softener_remaining=False)
+        self.modify(prog, supports_fabric_softener=True)
+
+    @Rule(
+        Drum(unbalanced_load=False, additional_water=False, jerking=False),
+        Door(closed=True, caught_laundry=False, water_leak=False),
+        DetergentDrawer(too_much_detergent=False, fabric_softener_remaining=False),
+        WaterOutlet(blocked=False, caught=False, jammed=False, connected_too_high=False, water_leak=False,
+                    damaged=False, connected_correctly=True),
+        DrainPipe(blocked=False, caught=False, jammed=False, unapproved_extension=False),
+        PowerCord(plugged_in=True),
+        Button(flashing=False),
+        Display(on=True, errors=[]),
+    )
+    def no_errors(self):
+        print("Everything is working fine.")
 
 
 if __name__ == "__main__":
--- KnowledgeBase/tests.py
+++ KnowledgeBase/tests.py
@@ -1,7 +1,30 @@
 from semantic_network import *
 
 
+def test0():
+    print("Test 0: Everything is working fine")
+
+    engine = SemanticNetwork()
+
+    engine.reset()
+
+    engine.declare(Drum(unbalanced_load=False, additional_water=False, jerking=False))
+    engine.declare(Door(closed=True, caught_laundry=False, water_leak=False))
+    engine.declare(DetergentDrawer(too_much_detergent=False, fabric_softener_remaining=False))
+    engine.declare(WaterOutlet(blocked=False, caught=False, jammed=False, connected_too_high=False, water_leak=False,
+                    damaged=False, connected_correctly=True))
+    engine.declare(DrainPipe(blocked=False, caught=False, jammed=False, unapproved_extension=False))
+    engine.declare(PowerCord(plugged_in=True))
+    engine.declare(Button(flashing=False))
+    engine.declare(Display(on=True, errors=[]))
+    engine.declare(Program())
+
+    engine.run()
+
+
 def test1():
+    print("Test 1: Various simple problems")
+
     engine = SemanticNetwork()
 
     engine.reset()
@@ -20,6 +43,8 @@ def test1():
 
 
 def test2():
+    print("Test 2: Water system problems")
+
     engine = SemanticNetwork()
 
     engine.reset()
@@ -27,8 +52,8 @@ def test2():
     engine.declare(Drum())
     engine.declare(Door())
     engine.declare(DetergentDrawer())
-    engine.declare(WaterOutlet(blocked=False, caught=True, jammed=False, connected_too_high=True, water_leak=True, damaged=True, connected_correctly=True))
-    engine.declare(DrainPipe(blocked=True, caught=False, jammed=True, unapproved_extension=False))
+    engine.declare(WaterOutlet(caught=True, connected_too_high=True, water_leak=True, damaged=True, connected_correctly=True))
+    engine.declare(DrainPipe(blocked=True, jammed=True))
     engine.declare(PowerCord())
     engine.declare(Button())
     engine.declare(Display(errors=['E:30 / -80']))
@@ -38,27 +63,89 @@ def test2():
 
 
 def test3():
+    print("Test 3: Multiple errors on the display")
+
+    engine = SemanticNetwork()
+
+    engine.reset()
+
+    engine.declare(Drum(unbalanced_load=True, additional_water=True))
+    engine.declare(Door(closed=False))
+    engine.declare(DetergentDrawer())
+    engine.declare(WaterOutlet(caught=True, connected_too_high=True))
+    engine.declare(DrainPipe())
+    engine.declare(PowerCord())
+    engine.declare(Button())
+    engine.declare(Display(errors=["E:60 / -2B", "E:30/-20", "E:30 / -80"]))
+    engine.declare(Program())
+
+    engine.run()
+
+
+def test4():
+    print("Test 4: Drum and detergent drawer problems")
+
+    engine = SemanticNetwork()
+
+    engine.reset()
+
+    engine.declare(Drum(unbalanced_load=True, additional_water=True))
+    engine.declare(Door())
+    engine.declare(DetergentDrawer(too_much_detergent=True, fabric_softener_remaining=True))
+    engine.declare(WaterOutlet())
+    engine.declare(DrainPipe())
+    engine.declare(PowerCord())
+    engine.declare(Button())
+    engine.declare(Display(errors=["E:30/-20", "E:60 / -2B"]))
+    engine.declare(Program(supports_fabric_softener=False))
+
+    engine.run()
+
+
+def test5():
+    print("Test 5: ")
+
     engine = SemanticNetwork()
 
     engine.reset()
 
     engine.declare(Drum())
-    engine.declare(Door())
+    engine.declare(Door(caught_laundry=True, water_leak=True))
     engine.declare(DetergentDrawer())
     engine.declare(WaterOutlet())
     engine.declare(DrainPipe())
     engine.declare(PowerCord())
-    engine.declare(Button())
-    engine.declare(Display())
+    engine.declare(Button(name="Start/Pause", flashing=True))
+    engine.declare(Display(on=False))
     engine.declare(Program())
+
+    engine.run()
 
 
 if __name__ == '__main__':
-    print("Test 1: Various simple problems")
+    test0()
+
+    print()
+    print()
+
     test1()
 
     print()
     print()
 
-    print("Test 2: Water system problems with all fields filled")
     test2()
+
+    print()
+    print()
+
+    test3()
+
+    print()
+    print()
+
+    test4()
+
+    print()
+    print()
+
+    test5()
